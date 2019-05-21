73a192ddae4f7105fd803b668d686d6b8384cf37
Toby Fox <tobyfox@gmail.com>
2019-05-20 20:41:20 -0400

    feat: stub boost component importing from @joincivil/components

---
+++ src/react/boosts/Boost.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { CivilLogo } from "@joincivil/components";
+
+export const Boost: React.FunctionComponent = () => {
+  return (
+    <b>This is a <CivilLogo /> boost.</b>
+  );
+};
