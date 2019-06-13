e1ac43a666bd921e78b0fed4ffdc74342c93a1a8
sruddy <sarah.ruddy@gmail.com>
2019-06-13 13:05:45 -0400

    fix: typos

--- src/react/boosts/payments/BoostPayForm.tsx
+++ src/react/boosts/payments/BoostPayForm.tsx
@@ -70,7 +70,7 @@ export class BoostPayForm extends React.Component<BoostPayFormProps, BoostPayFor
             <CheckboxLabel>Email me about progress of this Boost</CheckboxLabel>
           </BoostPayFormContain>
           <BoostFlexStart>
-            <SubmitInstructions>Once your Boost is sent, we’ll be sending you a confirmation email if selected of your completed transaction.  All procceded of the Boost go directly to the newsroom. If a Boost goal is not met, the proceeds will still go to fund the selected newsroom.</SubmitInstructions>
+            <SubmitInstructions>Once your Boost is sent, we’ll be sending you a confirmation email if selected of your completed transaction.  All proceeds of the Boost go directly to the newsroom. If a Boost goal is not met, the proceeds will still go to fund the selected newsroom.</SubmitInstructions>
             <div>
               <BoostButton type="submit" >Support this Boost</BoostButton>
               <SubmitWarning>Refunds are not possible. Civil does not charge any fees for this transaction. There are small fees charged by the Ethereum network. By sending a Boost, you agree to Civil’s Terms of Use and Privacy Policy. Depending on your selection, your email and comment may be visible to the newsroom.</SubmitWarning>
