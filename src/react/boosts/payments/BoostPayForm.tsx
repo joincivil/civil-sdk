0b136663700b9e6f201452943ab616b63364f995
sruddy <sarah.ruddy@gmail.com>
2019-06-25 14:31:57 -0400

    fix: move mutation to post transaction

--- src/react/boosts/payments/BoostPayForm.tsx
+++ src/react/boosts/payments/BoostPayForm.tsx
@@ -127,7 +127,7 @@ export class BoostPayForm extends React.Component<BoostPayFormProps, BoostPayFor
                 transactions={[
                   {
                     transaction: this.sendPayment,
-                    handleTransactionHash: this.handleTransactionHash,
+                    postTransaction: this.postTransaction,
                   },
                 ]}
                 modalContentComponents={PAY_MODAL_COMPONENTS}
@@ -159,7 +159,7 @@ export class BoostPayForm extends React.Component<BoostPayFormProps, BoostPayFor
     }
   };
 
-  private handleTransactionHash = async (txHash: TxHash) => {
+  private postTransaction = async (result: any, txHash: TxHash) => {
     await this.props.savePayment({
       variables: {
         postID: this.props.boostId,
